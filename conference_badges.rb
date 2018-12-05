d257406c2a0d6ead69c1ea8fdb8880d0ec1ecd0f
Simon Kong <simon.kong95@gmail.com>
2018-12-05 20:20:11 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,14 @@
 # Write your code here.
+
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(name)
+  counter = 0;
+  while 0 < name.length
+  name[counter] = "Hello, my name is #{name[counter]}."
+  counter += 1;
+  return name
+  end
+end
