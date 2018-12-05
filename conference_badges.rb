f6115958d5fc33c6c7cad951c2c35314db92a44e
Simon Kong <simon.kong95@gmail.com>
2018-12-05 20:18:58 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,18 @@
 # Write your code here.
+
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(name)
+  counter = 0;
+  while 0 < name.length
+  puts "Hello, my name is #{name[counter]}."
+  counter += 1;
+  end
+end
+
+def assign_rooms(name)
+
+end
+
